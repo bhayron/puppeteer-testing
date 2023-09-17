1d4ba3118dd427bc7ca3dd80a72ab008085b582b
bhayron <bhayronklivilan@gmail.com>
2023-09-17 00:06:36 -0300

    up

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const main = async () => {
     const browser = await puppeteer.launch({
         headless: false
     })
+    
     const page = await browser.newPage()
     await page.goto('https://www.netshoes.com.br/lst/mi-mais-vendidos?mi=hm_sc_f1_mais_vendidos_070923-mas-00&psn=Banner_Full_1')
     const clickElement = await page.waitForXPath('/html/body/div[1]/div/div[2]/div[2]/div[4]')
